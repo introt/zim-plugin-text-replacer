41b2738db11553ef30cb536929a425e45a997f21
introt <introt@koti.fimnet.fi>
2022-02-12 11:08:17 +0200

    First release

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# zim-text-replacer
-A barebones "Custom autocorrect"/text replacement plugin for Zim
+# Text Replacer
+
+A barebones "Custom autocorrect"/text replacement plugin for Zim.
+
+### How to use
+
+1. Move `textreplacer.py` into the plugins folder
+2. Create your replacements json (see `example.json` for reference) and copy its full path
+2. Enable the plugin in Zim and click "Configure"
+4. Paste in the path from step 2 and click "OK"
+
+From now on, every time you type one of the words you specified it will get replaced with the replacement you've chosen; this can be undone via undo (Ctrl+Z).
+
+To reload the json, toggle the plugin off and on again, or restart Zim.
+
+#### json format
+
+```json
+{
+	"coeur": "cœur",
+	"zw": "[[https://zim-wiki.org/|**Zim Desktop Wiki**]]"
+}
+```
+
+The json file should contain a single object with key-value pairs; the key is replaced by the value when typing. The keys shouldn't contain whitespace, but the values can. The values can also contain wiki formatting, though it isn't rendered before reload.
+
+#### Troubleshooting
+
+You can check the logs to confirm your replacements were successfully loaded. The following should show up:
+
+```
+INFO: Loaded n replacements from /your/replacements.json
+```
+
+If there's an error, n will be 0 and the above will be preceded by the encountered error:
+
+```
+ERROR: Failed to load json from bad path: [Errno 2] No such file or directory: 'bad path'
+```
---
+++ textreplace.py
@@ -0,0 +1,48 @@
+from json import load
+
+from zim.plugins import PluginClass
+from zim.gui.pageview import PageViewExtension
+
+import logging
+logger = logging.getLogger('zim.plugins.textreplacer')
+
+
+class TextReplacerPlugin(PluginClass):
+    plugin_info = {
+            'name': _('Text Replacer'),  # T: plugin name
+            'description': _('''\
+Text replacer allows you to define shortcuts for strings,
+similar to the built-in symbol replacement.
+
+With thanks to HorseLuvver and gandrille.
+'''),  # T: plugin description
+            'author': 'introt <introt@koti.fimnet.fi>'
+            }
+
+    plugin_preferences = (
+            ('json_path', 'string', _('Path to text replacements dictionary json file'), ''),  # T: plugin preference
+        )
+
+
+class TextReplacerPageViewExtension(PageViewExtension):
+    def __init__(self, plugin, pageview):
+        PageViewExtension.__init__(self, plugin, pageview)
+        self.replacements = {}
+        path = self.plugin.preferences['json_path']
+        try:
+            with open(path, 'r') as f:
+                self.replacements = load(f)
+        except Exception as e:
+            logger.error('Failed to load json from %s: %s', path, e)
+        finally:
+            logger.info('Loaded %s replacements from %s', len(self.replacements.keys()), path)
+            self.connectto(pageview.textview, 'end-of-word', self.on_end_of_word)
+
+    def on_end_of_word(self, textview, start, end, word, char, editmode):
+        buffer = textview.get_buffer()
+        if word in self.replacements.keys():
+            with buffer.tmp_cursor(start):
+                buffer.delete(start, end)
+                buffer.insert_at_cursor(self.replacements[word])
+            buffer.set_modified(True)
+            textview.stop_emission('end_of_word')
